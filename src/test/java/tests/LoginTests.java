bf00625d6246a92591cd38c61ed21d536465be54
abayramusa <abayramusa@gmail.com>
2019-02-01 20:32:16 -0600

    Git version 3

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -16,23 +17,26 @@ public class LoginTests {
 
     WebDriver driver;
 
-    @BeforeClass
-    public void setUp(){
+    @BeforeMethod
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+
+
     @Test
-    public void loginTest1(){
+    public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
+
     @AfterMethod
-    public void cleanUp(){
+    public void cleanUp() {
         driver.close();
     }
 }
